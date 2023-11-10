17cb92ab09883532e212192efd0d5f9bc33dc16f
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-10 11:17:28 +0100

    feat: snak2 completato

---
+++ JSnack2/myscript.js
@@ -0,0 +1,13 @@
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const parola1 = prompt("Scrivi la prima parola");
+const parola2 = prompt("Scrivi la seconda parola");
+
+if (parola1.length > parola2.length) {
+    console.log(parola1);
+    console.log(parola2);
+} else {
+    console.log(parola2);
+    console.log(parola1);
+}
