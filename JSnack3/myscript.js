46dd67125c83ef9d72f5f2d36c423c8e23296fa9
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-10 11:58:27 +0100

    feat: implementazione array

--- JSnack3/myscript.js
+++ JSnack3/myscript.js
@@ -4,10 +4,13 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 let totaleNumeri = 0;
+const elencoNumeri = []
 
 for (let i = 0; i < 10; i++) {
     const numeroUtente = Number(prompt(`Inserisci il tuo numero`));
+    elencoNumeri.push(numeroUtente);
     totaleNumeri += numeroUtente;
 }
 
+console.log("I numeri che hai inserito sono " + elencoNumeri);
 console.log("La somma dei numeri è " + totaleNumeri);
