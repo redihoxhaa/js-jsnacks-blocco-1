98dffa364f3dd0e36275313cc50a7bc6177cdd72
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-14 09:56:56 +0100

    feat: ciclo con while

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
 // Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è inferiore di 50.
 
+/*
+// WHILE
 const numbersArray = [];
 let numbersSum = 0;
 
@@ -20,3 +22,31 @@ console.log("I numeri che hai inserito sono: ")
 for (let i = 0; i < numbersArray.length; i++) {
     console.log(numbersArray[i])
 }
+*/
+
+/*
+
+// DO WHILE
+
+const numbersArray = [];
+let numbersSum = 0;
+
+do {
+    const userNumber = prompt("Inserisci il numero da sommare");
+    if (isNaN(userNumber)) {
+        alert("Per favore inserisici un numero in cifre")
+    } else {
+        numbersArray.push(userNumber);
+        numbersSum += +userNumber;
+    }
+}
+while (numbersSum < 50);
+
+console.log("La somma dei numeri che hai inserito è " + numbersSum);
+console.log("I numeri che hai inserito sono: ")
+
+for (let i = 0; i < numbersArray.length; i++) {
+    console.log(numbersArray[i])
+}
+*/
+
